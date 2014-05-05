73f6978564f2915f3cc0a4d880f48f5e86199435
Giuseppe Piscopo <piscopo.giuseppe@gmail.com>
2014-05-06 01:07:36 +0200

    Test: translated footer text and single quote in italian

--- webSource/test/includes/i18n/it.php
+++ webSource/test/includes/i18n/it.php
@@ -1,7 +1,7 @@
 <?php 
 $i18nResources['it'] = array(
     'some-page title' => "Una pagina di esempio in Italiano",
-	'some-page header' => "Una pagina d'esempio, in Italiano",
+	'some-page header' => "Una pagina d&rsquo;esempio, in Italiano",
 	'some-page lead' => "I contenuti di questa pagina provengono da risorse di testo tradotte in lingua",
 	'some-page content' => "Ecco il <em>contenuto principale</em> di questa pagina",
 	'some-page more content' => <<< EOS
@@ -10,13 +10,13 @@ Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus
 pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. 
 EOS
 	,
-	'some-page switch text' => "Passa all'altra lingua",
+	'some-page switch text' => "Passa all&rsquo;altra lingua",
 	'some-page switch url' => "../en",
-    'some-page look attribute' => "In aggiunta, dai un'occhiata nei sorgenti della pagina all'attributo <code>lang</code> dell'elemento <code>html</code>.",
-    'some-page go to another' => "Vai verso un'altra pagina",
+    'some-page look attribute' => "In aggiunta, dai un&rsquo;occhiata nei sorgenti della pagina all&rsquo;attributo <code>lang</code> dell&rsquo;elemento <code>html</code>.",
+    'some-page go to another' => "Vai verso un&rsquo;altra pagina",
 
     'another-page title' => "Altra pagina di esempio in Italiano",
-	'another-page header' => "Un'altra pagina d'esempio",
+	'another-page header' => "Un&rsquo;altra pagina d&rsquo;esempio",
 	'another-page lead' => "Ancora in Italiano",
 	'another-page content' => "Anche i contenuti di questa pagina provengono da risorse di testo tradotte.",
 	'another-page switch text' => "Cambia lingua",
@@ -24,7 +24,7 @@ EOS
 
 	'common meta description' => "Pagina di test del progetto Simple PHP Translate",
 	'common footer' => <<< EOS
-Shared with &#x2764; by <a href='http://www.braincrumbz.com' target='_blank' rel='nofollow'>BrainCrumbz</a>, 2014.
+Condiviso con &#x2764; da <a href='http://www.braincrumbz.com' target='_blank' rel='nofollow'>BrainCrumbz</a>, 2014.
 EOS
 	,
 );
