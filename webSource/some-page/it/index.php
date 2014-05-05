f30053f6ba60915934cf445025a8d084130feaf5
Giuseppe Piscopo <piscopo.giuseppe@gmail.com>
2014-05-05 22:35:05 +0200

    Added test description and basic sample page to translate

---
+++ webSource/some-page/it/index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="it">
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Test page for Simple PHP Translate project">
+    <meta name="author" content="BrainCrumbz">
+	<link rel="shortcut icon" href="../../public/ico/favicon.ico">
+	
+    <title>Some example page in Italian</title>
+	
+	<!-- Bootstrap core CSS -->
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+	<!-- Custom styles for sticky footer template -->
+    <link href="../../public/css/sticky-footer.css" rel="stylesheet">
+	
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="//oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="//oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+
+    <div class="container">
+      <div class="page-header">
+		<h1>Some example page, in Italian</h1>
+      </div>
+      <p class="lead">This page content will come from translated text resources.</p>
+      <p>
+        This page content.
+      </p>
+    </div>
+
+    <div id="footer">
+      <div class="container">
+        <p class="text-muted">Shared with &hearts; by <a href='http://www.braincrumbz.com' target='_blank' rel='nofollow'>BrainCrumbz</a>, 2014.</p>
+      </div>
+    </div>
+
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+
+</body>
+</html>
